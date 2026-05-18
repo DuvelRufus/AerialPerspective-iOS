915995a9319ef86ad725ac36f5d23ee655c932e2
Danny Axiotis <danny.anatta@gmail.com>
2026-05-18 17:15:48 +0200

    Initial Commit

---
+++ AerealPerspective/AerealPerspectiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  AerealPerspectiveApp.swift
+//  AerealPerspective
+//
+//  Created by Danny Axiotis on 2026-05-18.
+//
+
+import SwiftUI
+
+@main
+struct AerealPerspectiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
